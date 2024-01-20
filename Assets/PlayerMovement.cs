f50d03f3c432222fe81f67c026799d7203d1d1a4
Murray Smith <55713649+MurraySmith27@users.noreply.github.com>
2024-01-20 12:42:16 -0500

    Murrays work

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private InputAction moveAction;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveVector = moveAction.ReadValue<Vector2>().normalized * speed;
+
+        transform.Translate(new Vector3(moveVector.x, 0f, moveVector.y));
+
+    }
+    
+    
+    public void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    public void OnDisable()
+    {
+        moveAction.Disable();
     }
 }
